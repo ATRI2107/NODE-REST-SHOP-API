c9039e9f8a3b8f0305699ef2893ee5b557c7929a
ATRI2107 <atrichaturvedi@gmail.com>
2020-02-09 04:23:07 +0530

    JWT authorization middleware configured and added into the product routes for protetction

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,7 @@ const express=require("express");
 const router=express.Router();
 const mongoose=require('mongoose');
 const multer=require('multer');
+const checkAuth=require("../middleware/check-auth");
 const storage=multer.diskStorage({
     destination: function(req,file,cb){
         cb(null,'./uploads/');
@@ -60,7 +61,7 @@ router.get("/",(req,res,next)=>{
     });
 });
 
-router.post('/',upload.single('productImage'),(req,res,next)=>{
+router.post('/',upload.single('productImage'),checkAuth,(req,res,next)=>{
     console.log(req.file);
     const product=new Product({
         _id: new mongoose.Types.ObjectId,
