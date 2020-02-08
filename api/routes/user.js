84657ec451aae011141194ca6c8488e7a0e78caf
ATRI2107 <atrichaturvedi@gmail.com>
2020-02-09 01:34:08 +0530

    JWT generation added

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const mongoose=require("mongoose");
 const User=require("../models/user");
 const bcrypt=require("bcrypt");
+const jwt=require('jsonwebtoken');
 
 router.post("/signup",(req,res,next)=>{
     User.find({email: req.body.email})
@@ -49,6 +50,51 @@ router.post("/signup",(req,res,next)=>{
         }
     });
     
+});
+
+router.post("/login",(req,res,next)=>{
+    User.find({email: req.body.email})
+    .exec()
+    .then(user=>{
+        if(user.length<1)
+        {
+            return res.status(401).json({
+                message: "Authorization failed"
+            });
+        }
+        bcrypt.compare(req.body.password,user[0].password,(err,result)=>{
+            if(err)
+            {
+                return res.status(401).json({
+                    message: "Authorization Failed"
+                });
+            }
+            if(result)
+            {
+                const token=jwt.sign({
+                    email: user[0].email,
+                    _id: user[0]._id
+                },process.env.JWT_KEY,
+                    {
+                        expiresIn: "1h"
+                    }
+                );
+                return res.status(200).json({
+                    message: "Authorization successfull",
+                    token: token
+                });
+            }
+            res.status(401).json({
+                message: "Authorization Failed"
+            })
+        })
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 
 router.delete("/:userId",(req,res,next)=>{
